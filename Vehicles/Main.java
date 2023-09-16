be0f8ef0e9c2cfedf798614b0372129b8f2e8672
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-09-16 11:03:13 +0800

    Add Vehicle Prototype Design Pattern

---
+++ Vehicles/Main.java
@@ -0,0 +1,32 @@
+package Vehicles;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+
+    /*
+     * Video Reference: https://youtu.be/DcFhITC9v0E
+     */
+    public static void main(String[] args) {
+
+        List<Vehicle> vehicles = List.of(
+                new Car("car_brand", "car_model", "car_color", 300),
+                new Bus("bus_brand", "bus_model", "bus_color", 8)
+        );
+
+        List<Vehicle> copyList = new ArrayList<>();
+        for (Vehicle vehicle : vehicles) {
+            copyList.add(vehicle.clone());
+        }
+
+        copyList.forEach(System.out::println);
+
+        System.out.println("==========================================");
+
+        VehicleCache registry = new VehicleCache();
+        registry.put(vehicles);
+        System.out.println(registry.get("car_brand car_model"));
+
+    }
+
+}
---
+++ Vehicles/Vehicle.java
@@ -0,0 +1,30 @@
+package Vehicles;
+
+public abstract class Vehicle {
+
+    private final String brand;
+    private final String model;
+    private final String color;
+
+    public Vehicle(String brand, String model, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    protected Vehicle(Vehicle vehicle) {
+        this.brand = vehicle.brand;
+        this.model = vehicle.model;
+        this.color = vehicle.color;
+    }
+
+    public abstract Vehicle clone();
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel(){
+        return model;
+    }
+}
